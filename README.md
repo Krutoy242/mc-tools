1037c4bc6f9eaeb8b36a94466999a10d488ed0cc
Krutoy242 <Krutoy242@gmail.com>
2022-10-13 19:39:50 +0300

    build: misc dev environment changes

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 ```sh
 node --experimental-specifier-resolution=node --no-warnings --loader ts-node/esm packages/errors/src/cli.ts --log=packages/errors/test/debug.log
 ```
+
+
+https://www.kgajera.com/blog/how-to-test-yargs-cli-with-jest/
 -->
--- merge_jsons.js
+++ merge_jsons.js
@@ -1,12 +1,14 @@
 import { readFileSync, writeFileSync } from 'fs'
+import { resolve } from 'path'
 import merge from 'ts-deepmerge'
 
 // @ts-check
 
 const loadJson = f => JSON.parse(readFileSync(f, 'utf8'))
 
+const sourcePackagesJson = resolve(process.argv[2], '../../', 'package.json')
 const resultObj = merge.default(
-  loadJson('packages/package.json'),
+  loadJson(sourcePackagesJson),
   loadJson(process.argv[2])
 )
 
