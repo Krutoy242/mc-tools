4ba23e3dcf0210321e68f5e1c06eaa41b574d461
Krutoy242 <Krutoy242@gmail.com>
2022-10-16 15:34:31 +0300

    feat(errors): 🥅 better args handling and tests

--- packages/errors/src/cli.ts
+++ packages/errors/src/cli.ts
@@ -11,29 +11,37 @@ import { findErrors, parseBlacklist } from '.'
 ============================================= */
 const argv = yargs(process.argv.slice(2))
   .alias('h', 'help')
+  .detectLocale(false)
+  .scriptName('mct-errors')
+  .strict()
+  .version()
+  .option('output', {
+    alias    : 'o',
+    normalize: true,
+    describe : 'Path for output with errors. If not specified output into stdout.',
+  })
   .option('log', {
-    alias   : 'l',
-    type    : 'string',
-    describe: 'debug.log file path (may need to be enabled by launcher)',
-    default : 'logs/debug.log',
+    alias    : 'l',
+    default  : 'logs/debug.log',
+    describe : 'debug.log file path (may need to be enabled by launcher)',
+    normalize: true,
+    coerce   : (f: string) => {
+      if (!existsSync(f)) throw new Error(`${resolve(f)} doesnt exist. Provide correct path for debug.log`)
+      const log = readFileSync(f, 'utf8')
+      if (log.length < 100) throw new Error(`${resolve(f)} exist but too short. Probably wrong file.`)
+      return log
+    },
   })
   .option('blacklist', {
-    alias   : 'b',
-    type    : 'string',
-    describe: 'Path to .csv file with regExps',
+    alias    : 'b',
+    describe : 'Path to .csv file with regExps',
+    normalize: true,
   })
-  .option('output', {
-    alias   : 'o',
-    type    : 'string',
-    describe: 'Path for output with errors. If not specified output into stdout.',
+  .coerce('blacklist', (f: string) => {
+    if (!existsSync(f)) throw new Error(`${resolve(f)} doesnt exist. Provide correct path for with blacklist`)
+    return loadBlacklist(f)
   })
   .parseSync()
-
-if (!existsSync(argv.log))
-  throw new Error(`${resolve(argv.log)} doesnt exist. Provide correct path for debug.log`)
-
-if (argv.blacklist && !existsSync(argv.blacklist))
-  throw new Error(`${resolve(argv.blacklist)} doesnt exist. Provide correct path for .csv with blacklist`)
 
 /* =============================================
 =                                             =
@@ -49,14 +57,12 @@ function relative(relPath: string) {
 (async () => {
   const ignore: RegExp[] = []
 
-  if (argv.blacklist) ignore.push(...loadBlacklist(argv.blacklist))
+  if (argv.blacklist) ignore.push(...argv.blacklist)
 
   const defBLPath = relative('safe_errors.cfg')
   if (existsSync(defBLPath)) ignore.push(...loadBlacklist(defBLPath))
 
-  const dbgLogText = readFileSync(argv.log, 'utf8')
-
-  const unresolvedErrors = await findErrors(dbgLogText, ignore)
+  const unresolvedErrors = await findErrors(argv.log, ignore)
 
   if (unresolvedErrors.length)
     process.stdout.write(`Found ${unresolvedErrors.length} errors\n`)
--- packages/errors/test/cli.test.ts
+++ packages/errors/test/cli.test.ts
@@ -1,21 +1,41 @@
 import { execSync } from 'child_process'
 import { expect, test } from 'vitest'
 
-test('Cli app work', async () => {
-  const appResult = execSync(
-    'esno'
-    + ' src/cli.ts'
-    + ' --log=test/debug.log'
-    + ' --blacklist=test/known_errors.csv'
+function exec(params: string): string {
+  return execSync(
+    `esno src/cli.ts --log=test/debug.log ${params}`
   ).toString()
     .trim()
+}
 
-  expect(appResult).toBe(
-`Found 5 errors
-[main/ERROR] [mixin]: Mixin config mixins.spectral_edge.json does not specify "minVersion" property
-[main/ERROR] [mixin]: Mixin config mixins.spectral_edge.json does not specify "minVersion" property
-[Client thread/ERROR] [HadEnoughItems]: Failed to register mod categories: class lykrast.jetif.JETIFPlugin
-[Client thread/WARN] [smoothfont]: renderChar method might be replaced. Fix the space width to 4 (MC default).
-[Client thread/ERROR] [FML]: Unidentified mapping from registry minecraft:recipes`
-  )
+test('Run app no arguments', async () => {
+  const appResult = exec('').toString()
+    .trim()
+
+  expect(appResult).toMatchInlineSnapshot(`
+    "Found 6 errors
+    [main\\\\ERROR] [FML]: Coremod EyeOfDragonsPlugin: Unable to class load the plugin de.curlybracket.eyeofdragons.EyeOfDragonsPlugin
+    [main\\\\ERROR] [mixin]: Mixin config mixins.spectral_edge.json does not specify \\"minVersion\\" property
+    [main\\\\ERROR] [mixin]: Mixin config mixins.spectral_edge.json does not specify \\"minVersion\\" property
+    [Client thread\\\\ERROR] [HadEnoughItems]: Failed to register mod categories: class lykrast.jetif.JETIFPlugin
+    [Client thread\\\\WARN] [smoothfont]: renderChar method might be replaced. Fix the space width to 4 (MC default).
+    [Client thread\\\\ERROR] [FML]: Unidentified mapping from registry minecraft:recipes"
+  `)
+})
+
+test('Run app with additional filter', async () => {
+  const appResult = exec('--blacklist=test/custom_filter.cfg').toString()
+    .trim()
+
+  expect(appResult).toMatchInlineSnapshot(`
+    "Found 1 errors
+    [Client thread\\\\WARN] [smoothfont]: renderChar method might be replaced. Fix the space width to 4 (MC default)."
+  `)
+})
+
+test('Run app with file output', async () => {
+  const appResult = exec('--output=test/output.log').toString()
+    .trim()
+
+  expect(appResult).toMatchInlineSnapshot('"Found 6 errors"')
 })
