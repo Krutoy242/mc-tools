9e48cc3a3dde6be1e3b947f8cd6bac9a5f5dd7d8
Krutoy242 <Krutoy242@gmail.com>
2025-07-09 19:03:58 +0300

    build(errors): 🥅configure building script

--- packages/errors/test/cli.test.ts
+++ packages/errors/test/cli.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable style/no-tabs */
 import { execSync } from 'node:child_process'
 
 import { expect, it } from 'vitest'
@@ -5,35 +6,26 @@ import { expect, it } from 'vitest'
 function exec(params: string): string {
   return execSync(
     `tsx src/cli.ts --log=test/debug.log ${params}`
-  ).toString()
-    .trim()
+  ).toString().trim()
 }
 
 it('run app no arguments', async () => {
   const appResult = exec('')
 
   expect(appResult).toMatchInlineSnapshot(`
-    "Found 6 errors
-    [main\\\\ERROR] [FML]: Coremod EyeOfDragonsPlugin: Unable to class load the plugin de.curlybracket.eyeofdragons.EyeOfDragonsPlugin
-    [main\\\\ERROR] [mixin]: Mixin config mixins.spectral_edge.json does not specify \\"minVersion\\" property
-    [main\\\\ERROR] [mixin]: Mixin config mixins.spectral_edge.json does not specify \\"minVersion\\" property
-    [Client thread\\\\ERROR] [HadEnoughItems]: Failed to register mod categories: class lykrast.jetif.JETIFPlugin
-    [Client thread\\\\WARN] [smoothfont]: renderChar method might be replaced. Fix the space width to 4 (MC default).
-    [Client thread\\\\ERROR] [FML]: Unidentified mapping from registry minecraft:recipes"
-  `)
-})
-
-it('run app with additional filter', async () => {
-  const appResult = exec('--blacklist=test/custom_filter.cfg')
-
-  expect(appResult).toMatchInlineSnapshot(`
-    "Found 1 errors
-    [Client thread\\\\WARN] [smoothfont]: renderChar method might be replaced. Fix the space width to 4 (MC default)."
+    "Found 3 errors
+    [main/ERROR] [FML]: Coremod EyeOfDragonsPlugin: Unable to class load the plugin de.curlybracket.eyeofdragons.EyeOfDragonsPlugin
+    java.lang.ClassNotFoundException: de.curlybracket.eyeofdragons.EyeOfDragonsPlugin
+    	at java.net.URLClassLoader.findClass(URLClassLoader.java:381) ~[?:1.8.0_51]
+    	at net.minecraft.launchwrapper.LaunchClassLoader.findClass(LaunchClassLoader.java:117) ~[launchwrapper-1.12.jar:?]
+    	at java.lang.ClassLoader.loadClass(ClassLoader.java:424) ~[?:1.8.0_51]
+    	at java.lang.ClassLoader.loadClass(ClassLoader.java:357) ~[?:1.8.0_51]
+    [Client thread/ERROR] [HadEnoughItems]: Failed to register mod categories: class lykrast.jetif.JETIFPlugin
+    [Client thread/ERROR] [FML]: Unidentified mapping from registry minecraft:recipes"
   `)
 })
 
 it('run app with file output', async () => {
-  const appResult = exec('--output=test/output.log')
-
-  expect(appResult).toMatchInlineSnapshot('"Found 6 errors"')
+  expect(exec('--output=test/output.log'))
+    .toMatchInlineSnapshot(`"Found 3 errors"`)
 })
