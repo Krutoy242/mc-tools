f423fc62391d3ce84b389ab42de7c5556863a0cb
Krutoy242 <Krutoy242@gmail.com>
2025-08-01 14:20:01 +0300

    refactor(reducer): ✂️use `citty` for argument parser

--- packages/reducer/src/cli.ts
+++ packages/reducer/src/cli.ts
@@ -1,49 +1,60 @@
 #!/usr/bin/env node
 
+import type { ArgsDef, Resolvable} from 'citty'
+
 import { existsSync } from 'node:fs'
 import { resolve } from 'node:path'
 
-import yargs from 'yargs'
+import { defineCommand, runMain } from 'citty'
 
+import { description, name, version } from '../package.json'
 import { binary } from './binary'
 import { interactive } from './interactive'
 
-/* =============================================
-=                Arguments                    =
-============================================= */
 function assertPath(f: string, errorText?: string) {
   if (existsSync(f)) return f
   throw new Error(`${resolve(f)} ${errorText ?? 'doesnt exist. Provide correct path.'}`)
 }
 
-yargs(process.argv.slice(2))
-  .alias('h', 'help')
-  .detectLocale(false)
-  .scriptName('@mctools/reducer')
-  .strict()
-  .version()
-  .option('mc', {
-    alias    : 'm',
-    type     : 'string',
-    describe : 'Minecraft dir with mods/ and minecaftinstance.json',
-    default  : './',
-    normalize: true,
-    coerce   : assertPath,
-  })
-  .command({
-    command : 'binary',
-    describe: 'Reduce mods in half to find error',
-    builder : ya => ya,
-    handler : async (argv) => {
-      await binary(argv.mc)
-    },
-  })
-  .command({
-    command : 'interactive',
-    describe: 'Pick mods and manipulate them one by one',
-    builder : ya => ya,
-    handler : async (argv) => {
-      await interactive(argv.mc)
-    },
-  })
-  .parse()
+const cwdArg = {
+  cwd: {
+    type       : 'string',
+    alias      : 'm',
+    description: 'Minecraft dir with mods/ and minecaftinstance.json',
+    default    : './',
+  },
+} as Resolvable<ArgsDef>
+
+const main = defineCommand({
+  meta: {
+    name,
+    version,
+    description,
+  },
+  subCommands: {
+    binary: defineCommand({
+      meta: {
+        name       : 'binary',
+        description: 'Reduce mods in half to find error',
+      },
+      args: cwdArg,
+      async run({ args }) {
+        assertPath(args.cwd as string)
+        await binary(args.cwd as string)
+      },
+    }),
+    interactive: defineCommand({
+      meta: {
+        name       : 'interactive',
+        description: 'Pick mods and manipulate them one by one',
+      },
+      args: cwdArg,
+      async run({ args }) {
+        assertPath(args.cwd as string)
+        await interactive(args.cwd as string)
+      },
+    }),
+  },
+})
+
+runMain(main)
